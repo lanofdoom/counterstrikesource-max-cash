67a11ec00ff2076f9223c1e4a88c76fbba3e154f
Brad Weinberger <1484541+BradleyMarie@users.noreply.github.com>
2020-12-27 19:43:11 -0600

    Add build scripts

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -ue
+
+tools=https://sm.alliedmods.net/smdrop/1.10/sourcemod-1.10.0-git6502-linux.tar.gz
+
+tmp_dir=$(mktemp -d)
+echo using temporary folder: $tmp_dir >&2
+clean () {
+    rm -rf $tmp_dir
+    echo deleted temporary folder >&2
+}
+trap clean EXIT
+
+curl $tools -o $tmp_dir/tools.tar.gz
+tar -xf $tmp_dir/tools.tar.gz -C $tmp_dir
+$tmp_dir/addons/sourcemod/scripting/spcomp max_cash.sp
+
+echo created max_cash.smx
